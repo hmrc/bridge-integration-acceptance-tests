3414bd2b0232b4f9e66151f953b54c0591895714
peterdrobinson <77154273+peterdrobinson@users.noreply.github.com>
2026-05-21 13:54:59 +0100

    WIP

---
+++ src/test/scala/steps/helpers/RatepayerPropertiesStepHelper.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package steps.helpers
+
+import builders.{DashboardRequestBuilder, RatepayerPropertiesRequestBuilder}
+import models.{RatepayerStatusResponse, RegisterRatepayerResponse}
+import org.scalatest.matchers.should.Matchers
+import play.api.libs.json.JsValue
+import play.api.libs.ws.JsonBodyReadables.readableAsJson
+import play.api.libs.ws.StandaloneWSResponse
+import steps.context.{RatepayerPropertiesContext, RegisterContext}
+
+trait RatepayerPropertiesStepHelper { this: Matchers =>
+
+  def ratepayerPropertiesData(
+    context: RatepayerPropertiesContext,
+    credId: String
+  ): Unit = {
+    val response: StandaloneWSResponse = RatepayerPropertiesRequestBuilder.ratepayerProperties(credId)
+
+    val jsonResponseBody = response.body[JsValue]
+    context.responseBody = Some(jsonResponseBody.as[RegisterRatepayerResponse])
+    context.status = response.status
+    context.headers = response.headers.view.mapValues(_.mkString(", ")).toMap
+  }
+
+  def theResponseShouldContainTheFollowingDetails(
+    context: RatepayerPropertiesContext,
+    expectedResponse: RegisterRatepayerResponse
+  ): Unit = {
+    val actualResponseBody: Option[RegisterRatepayerResponse] = context.responseBody
+
+    context.status shouldBe 200
+    // actualResponseBody shouldBe Some(expectedResponse)
+  }
+
+}
