4da4817a9bbbd55e38768d732cd13cd92a4df30e
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2026-07-20 10:34:36 +0100

    [NGR-5739]Adding Search postcode acceptance test

---
+++ src/test/scala/uk/gov/hmrc/api/specs/SearchPostcodeFeatureSpec.scala
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.specs
+
+import models.RegisterRatepayerResponse
+import models.search.{PostcodeSearchResult, Record}
+import models.search.*
+import org.scalatest.featurespec.FixtureAnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{GivenWhenThen, Outcome}
+import steps.context.SearchPostcodeContext
+import steps.helpers.SearchPostcodeStepHelper
+
+class SearchPostcodeFeatureSpec
+    extends FixtureAnyFeatureSpec
+    with GivenWhenThen
+    with Matchers
+    with SearchPostcodeStepHelper {
+
+  override type FixtureParam = SearchPostcodeContext
+
+  override def withFixture(test: OneArgTest): Outcome = {
+    val context = SearchPostcodeContext()
+    try test(context)
+    finally ()
+  }
+
+  Feature("Search Postcode API Test") {
+
+    Scenario("Search postcode status response") { context =>
+      val personForeignId                        = "123456789567"
+      When(s"the get request is sent to the search postcode api with $personForeignId")
+      searchPostcode(context, personForeignId)
+      Then("the response should contain the following details")
+      val expectedResponse: PostcodeSearchResult =
+        PostcodeSearchResult(
+          results = Results(
+            current_page = None,
+            page_size = None,
+            total_results = None,
+            total_pages = None,
+            has_next = None,
+            has_previous = None,
+            self = None,
+            next = None,
+            prev = None,
+            first = None,
+            last = None,
+            records = List(
+              Record(
+                ValuationList(None, Classification(None, None), CollectionAuthority(None, None)),
+                ListEntry(RelevantProperty(None), Addresses(None), Valuation(None))
+              ),
+              Record(
+                ValuationList(None, Classification(None, None), CollectionAuthority(None, None)),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              ),
+              Record(
+                ValuationList(
+                  None,
+                  Classification(None, None),
+                  CollectionAuthority(None, None)
+                ),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              ),
+              Record(
+                ValuationList(
+                  None,
+                  Classification(None, None),
+                  CollectionAuthority(None, None)
+                ),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              ),
+              Record(
+                ValuationList(
+                  None,
+                  Classification(None, None),
+                  CollectionAuthority(None, None)
+                ),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              ),
+              Record(
+                ValuationList(
+                  None,
+                  Classification(None, None),
+                  CollectionAuthority(None, None)
+                ),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              ),
+              Record(
+                ValuationList(
+                  None,
+                  Classification(None, None),
+                  CollectionAuthority(None, None)
+                ),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              ),
+              Record(
+                ValuationList(
+                  None,
+                  Classification(None, None),
+                  CollectionAuthority(None, None)
+                ),
+                ListEntry(
+                  RelevantProperty(None),
+                  Addresses(None),
+                  Valuation(None)
+                )
+              )
+            )
+          )
+        )
+      theResponseShouldContainTheFollowingDetails(context, expectedResponse)
+    }
+
+  }
+
+}
